a064066999b41ea9260e046ebb3d99706c3f39bd
MelpCode <67125923+MelpCode@users.noreply.github.com>
2021-03-13 16:37:13 -0500

    Update settings.py

--- django_sessions_app/settings.py
+++ django_sessions_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's@x@gut_znj(gfp7a*^wi$19r5#^h)tlhkq)w%(^wlq$umnhag'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
